da5fc02639ea1736fe2ce8f3c0d21ac5d3a54fca
jvanhent <johnny.vanhentenryk@ixor.be>
2016-12-13 00:09:53 +0100

    Step 2: Let React Router handle Routing

---
+++ app/containers/Main.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var HelloWorld = React.createClass({
+    render: function() {
+        return ( 
+            <div> Hello ReactJS Program! </div>
+        )
+    }
+});
+
+module.exports = HelloWorld
