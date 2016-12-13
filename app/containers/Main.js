c58b7ec3352f15df2914d48c0d9260fb54a765e8
jvanhent <johnny.vanhentenryk@ixor.be>
2016-12-13 22:58:17 +0100

    Partial Step 3: Add UI for Home

--- app/containers/Main.js
+++ app/containers/Main.js
@@ -1,11 +1,40 @@
 var React = require('react');
 
-var HelloWorld = React.createClass({
-    render: function() {
-        return ( 
-            <div> Hello ReactJS Program! </div>
+var styles = {
+  container: {
+    height: '100%',
+    width: '100%'
+  },
+  header: {
+      marginBottom: "0px"
+  }
+
+}
+
+var Main = React.createClass({
+    render: function () {
+        return (
+            <div style={styles.container}>
+                <nav style={styles.header} className="navbar navbar-default">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <a className="navbar-brand" href="/">Brand</a>
+                        </div>
+
+                        <div className="collapse navbar-collapse">
+                            <form className="navbar-form navbar-right">
+                                <div className="form-group">
+                                    <input className="form-control" placeholder="Search"/>
+                                </div>
+                                <button type="submit" className="btn btn-default">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </nav>
+                {this.props.children}
+            </div>
         )
     }
 });
 
-module.exports = HelloWorld
+module.exports = Main
